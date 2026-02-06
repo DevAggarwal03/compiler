7f0142f33b71edd32124467dd980b1317b14c413
Dev Aggarwal <devaggarwal1009@gmail.com>
2026-02-07 00:24:18 +0530

    basic tokernizer done

--- src/Tokenizer.hpp
+++ src/Tokenizer.hpp
@@ -1,3 +1,160 @@
+#pragma once
+#include <cctype>
+#include <string>
 #include <iostream>
+#include <vector>
+#include <cstddef>
 
-using namespace std;
+// changing return type to optional
+// make this tokenKind {keyword, identifiers, Symbol, Integer, Eof}
+enum class TokenList {
+  _return,
+  int_lit,
+  semi,
+  variable,
+  _if,
+  _else,
+  _for,
+  _while,
+  _do,
+};
+
+struct Token {
+  // make a list of valid tokens
+  TokenList type;
+  std::string val;
+};
+
+struct CharReturnType {
+  bool isChar;
+  char val;
+};
+
+struct TokenReturnType {
+  bool istoken;
+  Token tkn;
+};
+
+class Tokenizer {
+
+private:
+  std::string code; 
+  std::size_t position = 0;
+  std::vector<Token> tokens;
+
+
+public:
+  explicit Tokenizer (const std::string& _code): code(_code){ 
+  }
+
+  TokenReturnType createAndReturnTkn(TokenList type, std::string& val){
+    Token newTkn = {.type = type, .val = val};
+    val = "";
+    return {true, newTkn};
+  }
+
+  // checkVariable
+  TokenReturnType check() {
+    std::string buffer = "";
+    CharReturnType c = peek(0); 
+    if(c.isChar && (c.val == '_' || std::isalpha(static_cast<unsigned char>(c.val)))){
+        buffer += consume().val;
+        c = peek();
+        while(c.isChar && (c.val == '_' || std::isalnum(static_cast<unsigned char>(c.val)))){
+            buffer += consume().val;
+            c = peek();
+        }
+
+        if(buffer == "return"){
+          return createAndReturnTkn(TokenList::_return, buffer);
+        }
+
+        if(buffer == "if"){
+          return createAndReturnTkn(TokenList::_if, buffer);
+        }
+
+        if(buffer == "else"){
+          return createAndReturnTkn(TokenList::_else, buffer);
+        }
+
+        if(buffer == "do"){
+          return createAndReturnTkn(TokenList::_do, buffer);
+        }
+
+        if(buffer == "while"){
+          return createAndReturnTkn(TokenList::_while, buffer);
+        }
+
+        if(buffer == "for"){
+          return createAndReturnTkn(TokenList::_for, buffer);
+        }
+
+        return createAndReturnTkn(TokenList::variable, buffer);
+    }
+
+    if(c.isChar && c.val == ';'){
+      buffer += consume().val;
+      return createAndReturnTkn(TokenList::semi, buffer);
+    }
+
+    if(c.isChar && c.val == ' '){
+      consume();
+      return {false};
+    }
+
+    if(c.isChar && isnumber(static_cast<unsigned char>(c.val))){
+      buffer += consume().val;
+      while(c.isChar && isnumber(static_cast<unsigned char>(c.val))){
+        buffer += consume().val;
+        c = peek();
+      }
+
+      return createAndReturnTkn(TokenList::int_lit, buffer);
+    }
+
+    return {false};
+  }
+
+
+  std::vector<Token> Tokenize() {
+    std::vector<Token> tokens;
+    while(!isEof()) {
+      if(peek().isChar && isspace(static_cast<unsigned char>(peek().val))){
+        consume();
+        continue;
+      }
+      TokenReturnType tkn = check();
+      if(tkn.istoken){
+        tokens.push_back(tkn.tkn);
+      }
+    }
+    return tokens;
+  }
+
+  // returns true if curr pos is equal to the length of the code. 
+  bool isEof() {
+    if(this -> position == this -> code.length()){
+      return true;
+    }
+    return false;
+  }
+
+  CharReturnType peek(const size_t idx = 0) const {
+    if(this -> position + idx >= code.length()){
+      return {false};
+    } 
+
+    return {true, code[this -> position + idx]};
+  }
+
+
+  CharReturnType consume() {
+    if(this->position >= this->code.length()){
+      return {false};
+    }
+    char _val = code[this->position];
+    position += 1;
+    return {true, _val};
+  }
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -5,78 +5,79 @@
 #include <ios>
 #include <iostream>
 #include <sstream>
+#include "./Tokenizer.hpp"
 
 using namespace std; 
 
-enum Token_list {
-  _return,
-  int_lit,
-  semi
-};
+// enum Token_list {
+//   _return,
+//   int_lit,
+//   semi
+// };
+//
+// struct Token {
+//   Token_list tokenType;
+//   string val = "";
+// };
+//
+// bool isDig(char c){
+//   return c >= '0' && c <= '9';
+// }
 
-struct Token {
-  Token_list tokenType;
-  string val = "";
-};
+// vector<Token> tokenize(string code){
+//   vector<Token> tokens;
+//   string exp = "";
+//   for(int i=0; i < code.size(); i++){
+//     if(isspace(code[i])){
+    //   continue;
+    // }
+    // if(isalpha(code[i])){
+    //   exp += code[i];
+    //   i += 1;
+    //   while(isalnum(code[i]) || code[i] == '_'){
+    //     exp+=code[i];
+    //     i+=1;
+    //   }
+    //   i -= 1;
 
-bool isDig(char c){
-  return c >= '0' && c <= '9';
-}
+      // // do more checks  
+      // if(exp == "return"){
+      //   tokens.push_back({.tokenType = Token_list::_return, .val = ""}); 
+      // }
 
-vector<Token> tokenize(string code){
-  vector<Token> tokens;
-  string exp = "";
-  for(int i=0; i < code.size(); i++){
-    if(isspace(code[i])){
-      continue;
-    }
-    if(isalpha(code[i])){
-      exp += code[i];
-      i += 1;
-      while(isalnum(code[i]) || code[i] == '_'){
-        exp+=code[i];
-        i+=1;
-      }
-      i -= 1;
-
-      // do more checks  
-      if(exp == "return"){
-        tokens.push_back({.tokenType = Token_list::_return, .val = ""}); 
-      }
-
-      exp = "";
-      continue;
-    }else if(code[i] == '-' || isDig(code[i])){
-      if(code[i] == '-' && (i+1 == code.length() || !isDig(code[i+1]))){
-        cerr << "invalid string at character " << i;
-        exit(EXIT_FAILURE) ;
-      }
-
-      exp+=code[i];
-      i++;
-
-      while(isDig(code[i])){
-        exp += code[i];
-        i++;
-      }
-      i -= 1;
-
-      tokens.push_back({.tokenType = Token_list::int_lit, .val = exp});
-
-      exp = ""; 
-      continue;
-    }else if(code[i] == ';'){
-      cout<<i<<endl;
-      tokens.push_back({.tokenType = Token_list::semi, .val = ";"});
-      continue;
-    }else{
-      cerr << "invalid string at character " << i;
-      exit(EXIT_FAILURE);
-    }
-
-  }
-    return tokens;
-}
+//       exp = "";
+//       continue;
+//     }else if(code[i] == '-' || isDig(code[i])){
+//       if(code[i] == '-' && (i+1 == code.length() || !isDig(code[i+1]))){
+//         cerr << "invalid string at character " << i;
+//         exit(EXIT_FAILURE) ;
+//       }
+//
+//       exp+=code[i];
+//       i++;
+//
+//       while(isDig(code[i])){
+//         exp += code[i];
+//         i++;
+//       }
+//       i -= 1;
+//
+//       tokens.push_back({.tokenType = Token_list::int_lit, .val = exp});
+//
+//       exp = ""; 
+//       continue;
+//     }else if(code[i] == ';'){
+//       cout<<i<<endl;
+//       tokens.push_back({.tokenType = Token_list::semi, .val = ";"});
+//       continue;
+//     }else{
+//       cerr << "invalid string at character " << i;
+//       exit(EXIT_FAILURE);
+//     }
+//
+//   }
+//     return tokens;
+// }
 
 int main(int argc, char* argv[]) {
 
@@ -93,13 +94,15 @@ int main(int argc, char* argv[]) {
   code = input.str();
   file.close();
 
-  vector<Token> tokens = tokenize(code);
-  cout<<tokens.size();
+  Tokenizer* Tokenizer = new class::Tokenizer(code);
+
+
+  vector<Token> tokens = Tokenizer -> Tokenize();
   for(Token tkn: tokens){
-    cout<<tkn.val<<" "<<tkn.tokenType<<endl;
+    cout<<tkn.val<<" "<<static_cast<int>(tkn.type)<<endl;
   }
 
-  // cout << "code: " << code << endl;
+  cout << "code: " << code << endl;
 
   return 0;
 }
